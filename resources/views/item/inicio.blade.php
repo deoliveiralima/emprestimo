8b61129ea14bbe2a4c8ab3e19fd60a95edec08ea
Igor Lima <deoliveiralimaigor@gmail.com>
2021-04-12 20:47:12 -0300

    avanço no modal de exclusao de itens

--- resources/views/item/inicio.blade.php
+++ resources/views/item/inicio.blade.php
@@ -26,6 +26,8 @@
                                 <td>
                                     <a  class="btn btn-success d-inline" href="{{route('item.edita', ['item'=>$item->id])}}" role="button">Modificar</a> 
                                     
+                                    <button id="btnModal" class="btn btn-primary" data-target-id="{{ $item->id }}" data-toggle="modal" > Excluir </button>
+                                    
                                     <form method="POST" class="d-inline" action="{{route('item.exclui', ['item'=>$item->id])}}">
                                         @csrf
                                         @method('delete')
@@ -40,10 +42,56 @@
             
                     </tbody>
                   </table>
+
+                  <div class="modal" id="modalConfirmaExclusaoItem" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title">Confirmar exclusão de: </h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        
+                        <div class="modal-body row d-flex justify-content-center" >
+                            <form action="/item/" id="formConfirmaExclusaoItem" class="form-horizontal" method="post">
+
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger">Excluir</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <div id="pass_id"></div>
+                            </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                
             </div>
           
 
     </div>
 </div>
+
+
+@endsection
+
+
+@section('script')
+<--!  Scrip para ativar o modal e passar o id do item que será excluido-->
+<script type="text/javascript">
+
+    
+$(document).on("click", "#btnModal", function () {
+     var myBookId = $(this).data('id');
+     $(".modal-body #bookId").val( myBookId );
+     alert('12'); 
+     // As pointed out in comments, 
+     // it is unnecessary to have to manually call the modal.
+     // $('#modalConfirmaExclusaoItem').modal('show');
+});
+
+</script>
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -50,6 +51,7 @@
 
         <main class="py-4">
             @yield('content')
+            @yield('script')
         </main>
     </div>
 </body>

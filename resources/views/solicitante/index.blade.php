75877ed892ec23f11baf99ae268fe62b4447669d
igor.lima <deoliveiralimaigor@gmail.com>
2021-04-15 14:39:49 -0300

    modificações no gerenciamento dos solicitantes

--- resources/views/solicitante/index.blade.php
+++ resources/views/solicitante/index.blade.php
@@ -18,7 +18,7 @@
                     </thead>
                     <tbody>
                         @foreach ($solicitantes as $solicitante)
-                            <tr class="clickable-row">
+                            <tr>
                                 <th scope="row">{{$solicitante->id}}</th>
                                 <td>{{$solicitante->nome}}</td>
                                 <td>{{$solicitante->email}}</td>
@@ -41,35 +41,7 @@
             </div>
     </div>
 </div>
-    <div class="modal" tabindex="-1" id="modalConfirmaExclusaoSolicitante" role="dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Confirmação</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
-            <div class="modal-body">
-            <p>Você tem certeza que deseja deletar este solicitante</p>
-            <p id="nomeSolicitante" class="h2"></p>
-            </div>
-            <div class="modal-footer">
-            
-            <form id="formExclusaoSolicitante" method="POST" action="">
-                @csrf
-            @method('delete')
-            <button type="submit" class="btn btn-danger">Excluir</button>
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Sair</button>
 
-            </form>
-            
-           
-            </div>
-        </div>
-        </div>
-    </div>
-
-
+@include('solicitante.modalexclusao')
+    
 @endsection
-
--- resources/views/solicitante/show.blade.php
+++ resources/views/solicitante/show.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="card" style="width: 18rem;">
+<div class="d-flex justify-content-center">
+    <div class="card" style="width: 80%;">
         <div class="card-body">
         <h5 class="card-title">{{$solicitante->nome}}}}</h5>
        
-        <p class="card-text">Nome: {{$solicitante->nome}}</p>
-        <p class="card-text">E-mail: {{$solicitante->email}} </p>
-        <p class="card-text">Contato: {{$solicitante->contato}} </p>
-
-        <a href="/solicitante/{{$solictante->id}}/edita" class="btn btn-primary">Alterar</a>
-        <a href="" class="card-link">Another link</a>
+        <p class="card-text h5">Nome: {{$solicitante->nome}}</p>
+        <p class="card-text h5">E-mail: {{$solicitante->email}} </p>
+        <p class="card-text h5">Contato: {{$solicitante->contato}} </p>
+        <div class="d-flex justify-content-start">
+        <a href="/solicitante/{{$solicitante->id}}/edita" class="btn btn-primary mr-3">Alterar</a>
+        <modal-confirma-solicitante solicitante-id="{{$solicitante->id}}" solicitante-nome="{{$solicitante->nome}}"> </modal-confirma>
+        </div>
         </div>
     </div>
 
+</div>
+
+@include('solicitante.modalexclusao')
 
 @endsection
